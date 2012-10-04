d73ae67d0681324c24e05da780e41aa0be2b6589
Devon Hjelm <devon@cs.unm.edu>
2012-10-04 14:12:27 -0600

    Compiles

---
+++ DBN/build/CMakeFiles/DBN.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/devon/Research/DBN/DBN/Connections.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/Connections.cpp.o"
+  "/Users/devon/Research/DBN/DBN/DBN.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/DBN.cpp.o"
+  "/Users/devon/Research/DBN/DBN/GaussianLayer.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/GaussianLayer.cpp.o"
+  "/Users/devon/Research/DBN/DBN/GradientDescent.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/GradientDescent.cpp.o"
+  "/Users/devon/Research/DBN/DBN/IO.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/IO.cpp.o"
+  "/Users/devon/Research/DBN/DBN/Layers.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/Layers.cpp.o"
+  "/Users/devon/Research/DBN/DBN/MLP.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/MLP.cpp.o"
+  "/Users/devon/Research/DBN/DBN/Pathway.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/Pathway.cpp.o"
+  "/Users/devon/Research/DBN/DBN/RBM.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/RBM.cpp.o"
+  "/Users/devon/Research/DBN/DBN/ReLULayer.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/ReLULayer.cpp.o"
+  "/Users/devon/Research/DBN/DBN/SigmoidLayer.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/SigmoidLayer.cpp.o"
+  "/Users/devon/Research/DBN/DBN/SoftmaxLayer.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/SoftmaxLayer.cpp.o"
+  "/Users/devon/Research/DBN/DBN/SupportFunctions.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/SupportFunctions.cpp.o"
+  "/Users/devon/Research/DBN/DBN/SupportMath.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/SupportMath.cpp.o"
+  "/Users/devon/Research/DBN/DBN/Teacher.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/Teacher.cpp.o"
+  "/Users/devon/Research/DBN/DBN/Timecourses.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/Timecourses.cpp.o"
+  "/Users/devon/Research/DBN/DBN/Types.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/Types.cpp.o"
+  "/Users/devon/Research/DBN/DBN/Viz.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/Viz.cpp.o"
+  "/Users/devon/Research/DBN/DBN/main.cpp" "/Users/devon/Research/DBN/DBN/build/CMakeFiles/DBN.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "SOURCE_DIR=\"/Users/devon/Research/DBN/DBN\""
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/System/Library/Frameworks/OpenGL.framework"
+  "/usr/local/include"
+  "/opt/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ DBN/build/CMakeFiles/DBN.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/DBN.dir/DBN.cpp.o"
+  "CMakeFiles/DBN.dir/GaussianLayer.cpp.o"
+  "CMakeFiles/DBN.dir/GradientDescent.cpp.o"
+  "CMakeFiles/DBN.dir/IO.cpp.o"
+  "CMakeFiles/DBN.dir/Layers.cpp.o"
+  "CMakeFiles/DBN.dir/Connections.cpp.o"
+  "CMakeFiles/DBN.dir/main.cpp.o"
+  "CMakeFiles/DBN.dir/MLP.cpp.o"
+  "CMakeFiles/DBN.dir/Pathway.cpp.o"
+  "CMakeFiles/DBN.dir/RBM.cpp.o"
+  "CMakeFiles/DBN.dir/ReLULayer.cpp.o"
+  "CMakeFiles/DBN.dir/SigmoidLayer.cpp.o"
+  "CMakeFiles/DBN.dir/SoftmaxLayer.cpp.o"
+  "CMakeFiles/DBN.dir/SupportFunctions.cpp.o"
+  "CMakeFiles/DBN.dir/SupportMath.cpp.o"
+  "CMakeFiles/DBN.dir/Teacher.cpp.o"
+  "CMakeFiles/DBN.dir/Timecourses.cpp.o"
+  "CMakeFiles/DBN.dir/Types.cpp.o"
+  "CMakeFiles/DBN.dir/Viz.cpp.o"
+  "DBN.pdb"
+  "DBN"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/DBN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
